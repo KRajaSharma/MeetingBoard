5e2bd384226b4ef33d43336d50fc1c924a0d461f
OPERATIVE\rsharma <rsharma@operative.com>
2020-12-18 11:18:25 +0530

    Initial Commit - Project Creation

---
+++ src/main/java/com/project/meetingboard/MeetingboardApplication.java
@@ -0,0 +1,13 @@
+package com.project.meetingboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/meetingboard/MeetingboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.meetingboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
