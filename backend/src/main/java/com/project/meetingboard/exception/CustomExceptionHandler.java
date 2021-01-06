514a52b469c9a7275b78757f3f8d78d4318de3d1
OPERATIVE\rsharma <rsharma@operative.com>
2021-01-06 11:23:04 +0530

    Added exception handler

---
+++ backend/src/main/java/com/project/meetingboard/exception/CustomExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.project.meetingboard.exception;
+
+import java.util.Date;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.MongoWriteException;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@ControllerAdvice
+@RestController
+@Slf4j
+public class CustomExceptionHandler {
+
+	@ExceptionHandler(DuplicateKeyException.class)
+	public final ResponseEntity<ExceptionResponse> handleEntityNotFoundException(Exception ex) {
+		log.error(ex.getMessage(), ex);
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "User with same emailId already exists", ex.getMessage());
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+}
