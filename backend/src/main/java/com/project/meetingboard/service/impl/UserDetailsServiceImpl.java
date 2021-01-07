35e23017fd337916776a73b969d406c818dfb860
OPERATIVE\rsharma <rsharma@operative.com>
2021-01-07 12:32:15 +0530

    Added password encoder while saving password.

--- backend/src/main/java/com/project/meetingboard/service/impl/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/project/meetingboard/service/impl/UserDetailsServiceImpl.java
@@ -1,6 +1,9 @@
 package com.project.meetingboard.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.project.meetingboard.dto.CreateUserRequestDTO;
@@ -8,16 +11,23 @@ import com.project.meetingboard.model.UserDetails;
 import com.project.meetingboard.repository.UserDetailsRepository;
 import com.project.meetingboard.service.UserDetailsService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
 	private UserDetailsRepository repository;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	@Override
 	public UserDetails createNewUser(CreateUserRequestDTO request) {
 
-		return repository.save(new UserDetails(request.getEmailId(), request.getPassword(), null));
+		log.debug("create new user service impl");
+		return repository.save(new UserDetails(request.getEmailId(), encoder.encode(request.getPassword()), new ArrayList<>()));
 		
 		
 	}
