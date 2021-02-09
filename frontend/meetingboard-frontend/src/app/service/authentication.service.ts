dd8e1282d68e59bfc1834cc66ef769ab2a6d2b54
OPERATIVE\rsharma <rsharma@operative.com>
2021-02-09 12:43:18 +0530

    Misc

--- frontend/meetingboard-frontend/src/app/service/authentication.service.ts
+++ frontend/meetingboard-frontend/src/app/service/authentication.service.ts
@@ -8,12 +8,13 @@ import { map } from 'rxjs/operators';
 export class AuthenticationService {
 
   constructor(private httpClient: HttpClient) { }
-
   authenticate(username, password) {
     const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
     return this.httpClient.get('http://localhost:8080/v1/login', { headers })
       .pipe(map(userData => {
         sessionStorage.setItem('username', username);
+        let authString = 'Basic ' + btoa(username + ':' + password);
+        sessionStorage.setItem('basicauth', authString);
         return userData;
       }));
   }
