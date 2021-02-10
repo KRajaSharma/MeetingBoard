0dc07db8af8223ec3ff39de2d925f0958fddbba8
OPERATIVE\rsharma <rsharma@operative.com>
2021-02-10 15:06:47 +0530

    Misc

--- frontend/meetingboard-frontend/src/app/app-routing.module.ts
+++ frontend/meetingboard-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 import { LoginComponent } from './login/login.component';
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path:'',component:LoginComponent},
   {path:'register',component:RegisterComponent},
   {path:'forgotpassword',component:ForgotpasswordComponent},
-  {path:'reset',component:ResetpasswordComponent}
+  {path:'reset',component:ResetpasswordComponent},
+  {path:'board/:id',component:BoardComponent}
 ];
 
 @NgModule({
--- frontend/meetingboard-frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/meetingboard-frontend/src/app/dashboard/dashboard.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { SummaryBoards } from '../model/summaryBoard';
 import { BoardService } from '../service/board.service';
 import { UserServiceService } from '../service/user-service.service';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { LogoutDialogComponent } from '../logout-dialog/logout-dialog.component';
 import { Router } from '@angular/router';
-
+import { NewBoardDialogComponent } from '../new-board-dialog/new-board-dialog.component';
+import { Board } from '../model/board';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,23 +15,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  summaryBoards : SummaryBoards[];
-  user:string;
-  constructor(private boardService: BoardService,public dialog:MatDialog,private router:Router) { }
+  summaryBoards: SummaryBoards[];
+  user: string;
+  newBoard:Board;
+  constructor(private boardService: BoardService, public dialog: MatDialog, private router: Router) { }
 
   ngOnInit(): void {
     this.user = sessionStorage.getItem('username');
     this.boardService.getSummaryBoards().subscribe(
-      data=>{this.summaryBoards = data},
-      error=>{console.error(error)}
+      data => { this.summaryBoards = data },
+      error => { console.error(error) }
     )
   }
 
-  openLogoutDialog(){
+  openLogoutDialog() {
     console.log("logout");
     const dialogRef = this.dialog.open(LogoutDialogComponent);
     dialogRef.afterClosed().subscribe(result => {
-      if(result){
+      if (result) {
         console.log("Logging Out");
         sessionStorage.clear();
         this.router.navigate(['']);
@@ -37,8 +40,34 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  test(id){
+  addNewBoard() {
+    console.log("New Board");
+    let boardId:string;
+    let board = new Board(null, "", "", true, formatDate(new Date(), 'yyyy/MM/dd', 'en'), [], [], []);
+    const dialogRef = this.dialog.open(NewBoardDialogComponent, {
+      data: board
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log("Creating New Board");
+        this.boardService.upsertBoard(board).subscribe(
+          record=>{
+            this.newBoard = record;
+            this.router.navigate(['board/'+this.newBoard.id])
+          },
+          error=>{console.error(error)}
+        )
+      }
+    });
+
+  }
+
+  openBoard(id){
+    this.router.navigate(['board/'+id]);
+  }
+
+  test(id) {
     console.log(id);
     console.log("test");
   }
- }
+}
